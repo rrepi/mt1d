1f531f07bfdb08ab9b20461a7070e6a5b7fa6fa6
rrepi <raffael.ott@helmholtz-munich.de>
2025-03-28 14:31:13 +0100

    01_EWAS.R

--- 01_EWAS.R
+++ 01_EWAS.R
@@ -1,11 +1,11 @@
 ### EWAS analysis
 # INPUT:
-# Methylation M-value matrix (rows=CpGs, columns=sampleID)
-# Metadata file including the sampleID, covariables and
+# Methylation M-value matrix (rows = CpG probe IDs, columns = sample ID)
+# Metadata file including the sample ID, co-variables (including batch variables) and
 # estimated blood cell types (cd4t, cd8t, nk, mono, neu, bcell)
+# A dummy dataset to run the code is provided at https://github.com/rrepi/mt1d
 
 #load packages
-library(readxl)
 library(tableone)
 library(dplyr)
 library(matrixStats)
@@ -25,7 +25,7 @@ IQR.removal <- function(meth)
   meth
 }
 
-#Ewas function
+#EWAS function using robust linear regression
 ewas.function <-  function(meth, pheno, variable.of.interest)
   {
   model.covariates <- colnames(pheno)[-which(colnames(pheno) %in% c(variable.of.interest,"sample.id"))]
@@ -42,32 +42,37 @@ ewas.function <-  function(meth, pheno, variable.of.interest)
 }
 
 #data path
-setwd() #set path for stored data
+setwd("") #set path for stored data
 
 #load methylation Mvalues
-load("Mval.RData") #name according to your methylation matrix
+Mval <- read.table("test_mvalues.txt", 
+                   header = TRUE) #name according to your methylation matrix
 
-# load and prepare metadata
-metadata <- read_excel("Pheno.xlsx") %>%
+#load and prepare metadata
+metadata <- read.table("test_metadata.txt", 
+                       header = TRUE) %>%
   mutate_at(vars(age, PC1, PC2, PC3, cd8t:neu), as.numeric) %>%
   mutate(sex = ifelse(sex == 0, "Male", "Female"))
-metadata$maternal_t1d<-as.factor(metadata$maternal_t1d)
+metadata$maternal_t1d <- as.factor(metadata$maternal_t1d)
 
 #check matching and ordering of methylation matrix and metadata
-Mval<-Mval[,as.character(colnames(Mval)) %in% as.character(metadata$sample_ID)]
-Mval<-Mval[,order(match(as.character(colnames(Mval)), as.character(metadata$sample_ID)))]
+Mval<-Mval[,as.character(colnames(Mval)) %in% as.character(metadata$subject_ID)]
+Mval<-Mval[,order(match(as.character(colnames(Mval)), as.character(metadata$subject_ID)))]
 Mval<-as.matrix(Mval)
 
 #run EWAS
 cell.names <- c("cd8t", "cd4t", "nk", "bcell", "mono", "neu")
 covs <- c("maternal_t1d","age", "sex", "PC1", "PC2", "PC3", cell.names)
 
-Mval <- IQR.removal(Mval)
+#Mval <- IQR.removal(Mval) #not needed for the dummy dataset
+
 res <- ewas.function(
-    meth=Mval, 
-    pheno=metadata[,colnames(metadata) %in% covs], 
+    meth = Mval, 
+    pheno = metadata[,colnames(metadata) %in% covs], 
     variable.of.interest = "maternal_t1d")
 
 #add P FDR according to Benjamini-Hochberg
-res$P.fdr<-p.adjust(res$p, method = "BH")
-res<-res[order(res$P.fdr),]
+res$P.fdr <- p.adjust(res$p, method = "BH")
+res <- res[order(res$P.fdr),]
+
+#End
